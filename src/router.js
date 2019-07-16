b39eb05ed2d136121b2be584438db275452ebd73
Eddie Lewis <eddie.f.lewis@protonmail.com>
2019-07-16 17:45:44 +0100

    Changed routes from boiler plate to own components

--- src/router.js
+++ src/router.js
@@ -1,23 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import TodoList from './views/TodoList.vue'
+import Settings from './views/Settings.vue'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'todoList',
+      component: TodoList
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/settings',
+      name: 'settings',
+      component: Settings
     }
   ]
 })
