b1ff2b57025082cd78cc2842a23d547a4c26f2f2
Eddie Lewis <eddie.f.lewis@protonmail.com>
2019-07-18 16:55:22 +0100

    Use icon components instead of raw svgs. Testing bullet points

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,86 +1,84 @@
 <template>
-  <div class="todo-item card flex-row" v-bind:class="{'is-complete':todo.completed}">
-    <span class="p-3 mr-auto">{{ todo.title }}</span>
-    <div class="buttons flex-grow-0 d-flex">
-      <button @click="$emit('del-todo', todo.id)" class="remove border-0 bg-transparent h-100">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          xmlns:xlink="http://www.w3.org/1999/xlink"
-          x="0px"
-          y="0px"
-          viewBox="0 0 22 22"
-          xml:space="preserve"
-        >
-          <g>
-            <g>
-              <path
-                class="fill"
-                d="M16.1,3.6h-1.9V3.3c0-1.3-1-2.3-2.3-2.3h-1.7C8.9,1,7.8,2,7.8,3.3v0.2H5.9c-1.3,0-2.3,1-2.3,2.3v1.3c0,0.5,0.4,0.9,0.9,1v10.5c0,1.3,1,2.3,2.3,2.3h8.5c1.3,0,2.3-1,2.3-2.3V8.2c0.5-0.1,0.9-0.5,0.9-1V5.9C18.4,4.6,17.4,3.6,16.1,3.6z M9.1,3.3c0-0.6,0.5-1.1,1.1-1.1h1.7c0.6,0,1.1,0.5,1.1,1.1v0.2H9.1V3.3z M16.3,18.7c0,0.6-0.5,1.1-1.1,1.1H6.7c-0.6,0-1.1-0.5-1.1-1.1V8.2h10.6L16.3,18.7L16.3,18.7z M17.2,7H4.8V5.9c0-0.6,0.5-1.1,1.1-1.1h10.2c0.6,0,1.1,0.5,1.1,1.1V7z"
-              />
-            </g>
-            <g>
-              <g>
-                <path
-                  class="fill"
-                  d="M11,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6s0.6,0.3,0.6,0.6v6.8C11.6,17.7,11.4,18,11,18z"
-                />
-              </g>
-              <g>
-                <path
-                  class="fill"
-                  d="M8,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8C7.4,10.2,7.7,10,8,10c0.4,0,0.6,0.3,0.6,0.6v6.8C8.7,17.7,8.4,18,8,18z"
-                />
-              </g>
-              <g>
-                <path
-                  class="fill"
-                  d="M14,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C14.6,17.7,14.3,18,14,18z"
-                />
-              </g>
-            </g>
-          </g>
-        </svg>
-      </button>
-      <button @click="markComplete" class="complete border-0 bg-transparent h-100">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          xmlns:xlink="http://www.w3.org/1999/xlink"
-          x="0px"
-          y="0px"
-          viewBox="0 0 22 22"
-          xml:space="preserve"
-        >
-          <rect y="0" class="no-fill" width="22" height="22" />
-          <g>
-            <path
-              class="fill"
-              d="M9.7,14.4L9.7,14.4c-0.2,0-0.4-0.1-0.5-0.2l-2.7-2.7c-0.3-0.3-0.3-0.8,0-1.1s0.8-0.3,1.1,0l2.1,2.1l4.8-4.8
-		c0.3-0.3,0.8-0.3,1.1,0s0.3,0.8,0,1.1l-5.3,5.3C10.1,14.3,9.9,14.4,9.7,14.4z"
-            />
-          </g>
-        </svg>
-      </button>
+  <div class="todo-item card flex-column p-3" v-bind:class="{'is-complete':todo.completed}">
+    <b-form-input id="title" class="mb-3 border-0 shadow-none" v-model="todoTitle"></b-form-input>
+    <div class="flex-row d-flex">
+      <div class="flex-column d-flex">
+        <div class="hide" v-for="value in this.rows"></div>
+      </div>
+      <b-form-textarea
+        @input="onInput"
+        id="body"
+        class="mb-auto border-0 shadow-none py-0"
+        v-model="todo.title"
+        rows="2"
+        max-rows="12"
+        no-resize
+        no-auto-shrink
+      ></b-form-textarea>
+    </div>
+    <div>
+      <div class="buttons flex-grow-0 d-flex justify-content-between">
+        <button @click="$emit('del-todo', todo.id)" class="remove border-0 bg-transparent">
+          <DeleteIcon />
+        </button>
+        <button @click="$emit('del-todo', todo.id)" class="remove border-0 bg-transparent">
+          <DeleteIcon />
+        </button>
+        <button @click="markComplete" class="complete border-0 bg-transparent">
+          <CompleteIcon />
+        </button>
+      </div>
     </div>
   </div>
 </template> 
 
 <script>
+import DeleteIcon from "./icons/Delete.vue";
+import CompleteIcon from "./icons/Complete.vue";
+
 export default {
   name: "TodoItem",
   props: ["todo"],
+  components: {
+    DeleteIcon,
+    CompleteIcon
+  },
   methods: {
     markComplete() {
       this.todo.completed = !this.todo.completed;
+    },
+    onInput(e) {
+      this.rows = e.split("\n").length;
+      console.log(this.rows);
     }
+  },
+  data() {
+    return {
+      todoTitle: "Your title here",
+      rows: 1
+    };
   }
 };
 </script>
 
 <style scoped>
+.hide {
+  width: 10px;
+  height: 10px;
+  margin: 5px 0;
+  background-color: #e85656;
+}
+textarea {
+  line-height: 20px;
+}
+input#title {
+  font-size: 1.25rem;
+  font-weight: 600;
+}
 .is-complete {
   text-decoration: line-through;
 }
-.buttons button:not(:first-child)::before {
+.abuttons button:not(:first-child)::before {
   content: "";
   width: 1px;
   height: 30px;
@@ -91,49 +89,8 @@ export default {
 }
 
 button {
-  width: 50px;
+  height: 32px;
+  width: 32px;
   position: relative;
-}
-
-button svg {
-  width: 22px;
-  height: 22px;
-  top: calc(50% - 11px);
-  left: calc(50% - 11px);
-  position: absolute;
-}
-
-.buttons button svg .fill {
-  transition: fill 0.2s ease;
-}
-
-button.remove svg {
-  fill: #c0cecb;
-}
-
-button.remove:hover svg {
-  fill: #e85656;
-}
-
-button.complete svg {
-  border-radius: 11px;
-  border: 1.5px solid #25b99a;
-  transition: background 0.2s ease;
-}
-
-button.complete svg .fill {
-  fill: #25b99a;
-}
-
-button.complete:hover svg {
-  background: rgba(37, 185, 154, 0.5);
-}
-
-button.complete:hover svg .fill {
-  fill: #ffffff;
-}
-
-svg .no-fill {
-  fill: none;
 }
 </style>
